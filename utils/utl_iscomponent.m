237b07f9a6b895422ce919ea89c2e12e0968d0c2
Laurens R Krol <lrkrol+git@gmail.com>
2017-12-01 22:06:39 +0100

    added utl_iscomponent

---
+++ utils/utl_iscomponent.m
@@ -0,0 +1,42 @@
+% iscomponent = utl_iscomponent(c)
+%
+%       Returns true when variable c is a struct with all four of the
+%       required component fields.
+%
+% In:
+%       c - the variable to be checked
+%
+% Out:  
+%       iscomponent - boolean, true when c is a struct with all four of the
+%                     required component fields
+% 
+%                    Copyright 2017 Laurens R Krol
+%                    Team PhyPA, Biological Psychology and Neuroergonomics,
+%                    Berlin Institute of Technology
+
+% 2017-12-01 First version
+
+% This file is part of Simulating Event-Related EEG Activity (SEREEGA).
+
+% SEREEGA is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+
+% SEREEGA is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+
+% You should have received a copy of the GNU General Public License
+% along with SEREEGA.  If not, see <http://www.gnu.org/licenses/>.
+
+function iscomponent = utl_iscomponent(c)
+
+if isstruct(c) && all(isfield(c, {'source', 'signal', 'orientation', 'orientationDv'}))
+    iscomponent = true;
+else
+    iscomponent = false;
+end 
+
+end
