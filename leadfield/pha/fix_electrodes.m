61366bbda4fc38035fbea06afa17d76a12b71a6d
Laurens R Krol <lrkrol+git@gmail.com>
2018-03-23 14:16:26 -0700

    Added leadfields: Pediatric Head Atlases

---
+++ leadfield/pha/fix_electrodes.m
@@ -0,0 +1,205 @@
+% [chanlocs, headmidpoint, rotationX, rotationZ, shift] = fix_electrodes(atlas, layout)
+%       
+%       Fixes the electrode recording such that the electrode grid fits
+%       onto the standard head model
+%
+%       Assumes you have the requested Pediatric Head Atlas directories in
+%       your MATLAB path. In particular, it needs the respective 
+%       Sensors_*.txt files. 
+%       As of 2018-02-05, the Pediatric Head Atlases are available upon 
+%       request at https://pedeheadmod.net
+%       
+%       Pediatric Head Atlas publication:
+%           Song, J., Morgan, K., Turovets, S., Li, K., Davey, C.,
+%           Govyadinov, P., Tucker, D. M. (2013). Anatomically accurate
+%           head models and their derivatives for dense array EEG source
+%           localization. Functional Neurology, Rehabilitation, and
+%           Ergonomics, 3(2-3), 275-293.
+%
+% In:
+%       atlas - string indicating the atlas to use: '0to2', '4to8', or
+%               '8to12'.
+%       layout - string indicating which layout (i.e. number of channels)
+%                to use: '128', '256', or '2562'. note: '256' is not 
+%                available for atlas '0to2'. 
+%
+% Out: 
+%       chanlocs          - the shifted, fixed channel locations to the
+%                           given electrode file
+%       headmidpoint      - the correct head midpoint, calculated from the
+%                           fiducials
+%       rotationX         - matrix for the rotation around the X-axis, such
+%                           that the ears are on the same line
+%       rotationZ         - matrix for the rotation around the Z-axis, such
+%                           that the nose direction aligns with the
+%                           corresponding axis
+%       shift             - the hard-coded method to fit the electrode
+%                           grid onto the standard EEGLAB head model
+% Usage example:
+%       >> [chans, hm, A1, A2, shift] = fix_Electrodes('8to18', '2562')
+% 
+%                    Copyright 2018 Juliane Pawlitzki
+%                    Team PhyPA, Biological Psychology and Neuroergonomics,
+%                    Berlin Institute of Technology
+
+% 2018-03-22 First version jpaw
+
+% This file is part of Simulating Event-Related EEG Activity (SEREEGA).
+
+% SEREEGA is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+
+% SEREEGA is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+
+% You should have received a copy of the GNU General Public License
+% along with SEREEGA.  If not, see <http://www.gnu.org/licenses
+
+function [chans, hm, A1, A2, shift] = fix_electrodes(atlas, layout)
+%% preparation
+
+if strcmp(layout,'2562')
+    layoutchan='generic';
+else
+    layoutchan=layout;
+end
+sensorfile = sprintf('Sensors_%s_%s.txt', atlas, layoutchan);
+chanlocs = readlocs(sensorfile, 'filetype', 'sfp');
+
+A1=eye(3);
+
+%% shifting the head midpoint: all
+%get fiducials
+[~, chanidx] = ismember({'FidNz', 'FidT9', 'FidT10'}, {chanlocs.labels});
+nas=[chanlocs(chanidx(1)).X, chanlocs(chanidx(1)).Y, chanlocs(chanidx(1)).Z];
+lpa=[chanlocs(chanidx(2)).X, chanlocs(chanidx(2)).Y, chanlocs(chanidx(2)).Z];
+rpa = [chanlocs(chanidx(3)).X, chanlocs(chanidx(3)).Y, chanlocs(chanidx(3)).Z];
+
+%calculate head midpoint from fiducials
+u = lpa-rpa;
+
+e = u(1)*nas(1) + u(2)*nas(2) + u(3)*nas(3);
+r = (e-u(1)*lpa(1)-u(2)*lpa(2)-u(3)*lpa(3))/(u(1)^2+u(2)^2+u(3)^2);
+
+%new head midpoint
+hm=lpa+r*u;
+
+%shift all coordinates to the head midpoint as new origin
+for i=1:length(chanlocs)
+   chanlocs(i).X = chanlocs(i).X-hm(1); 
+   chanlocs(i).Y = chanlocs(i).Y-hm(2);
+   chanlocs(i).Z = chanlocs(i).Z-hm(3);
+end
+
+%% rotating along the x-axis (ears on same z): not 2562
+if (~strcmp(layout,'2562'))
+    %set new fiducials, shifted to the new head midpoint
+    lpa = [chanlocs(chanidx(2)).Y, chanlocs(chanidx(2)).X, chanlocs(chanidx(2)).Z];
+    rpa = [chanlocs(chanidx(3)).Y, chanlocs(chanidx(3)).X, chanlocs(chanidx(3)).Z];
+
+    %determine the direction of shift
+    if lpa(3)>rpa(3) 
+        shift = 1;              %shift clockwise
+    else
+        shift = -1;
+    end
+
+    l=lpa; r=rpa;
+    radl = norm(lpa);           %radius of lpa
+    radr = norm(rpa);
+
+    %set z, x
+    l(3) = lpa(3)+(rpa(3)-lpa(3))/2;
+    r(3) = rpa(3)+(lpa(3)-rpa(3))/2;
+
+    l(1) = sign(lpa(1))*abs(sqrt(radl^2-lpa(2)^2-l(3)^2));
+    r(1) = sign(rpa(1))*abs(sqrt(radr^2-rpa(2)^2-r(3)^2));
+
+    %get rotation angles and define rotation matrix
+    alpha1 = atan2d(norm(cross(l,lpa)),dot(l,lpa));
+    alpha2 = atan2d(norm(cross(r,rpa)),dot(r,rpa));
+    alpha=(alpha1+alpha2)/2;
+
+    A1=[cosd(alpha),0,shift*sind(alpha);0,1,0;shift*-sind(alpha),0,cosd(alpha)];
+
+    %rotate all coordinates by alpha, with A
+    for i=1:length(chanlocs)
+       chan=A1*[chanlocs(i).X; chanlocs(i).Y; chanlocs(i).Z];
+       chanlocs(i).X=chan(1);
+       chanlocs(i).Y=chan(2);
+       chanlocs(i).Z=chan(3);
+    end
+end
+
+%% rotating along the z-axis (direction of nose)
+%set new fiducials, shifted to the new head midpoint
+nas=[chanlocs(1).X, chanlocs(1).Y, chanlocs(1).Z];
+
+if strcmp(layout,'2562')||strcmp(atlas, '0to2')
+    [~, chanidx] = max([chanlocs.X]);
+    nas = [chanlocs(chanidx).X, chanlocs(chanidx).Y, chanlocs(chanidx).Z];
+end
+
+alpha=90-atan2d(norm(cross(nas,[0,1,0])),dot(nas,[0,1,0]));
+shiftdir=1;
+if strcmp(atlas,'8to18')
+    shiftdir=-1;
+end
+
+A2=[cosd(alpha), shiftdir*-sind(alpha), 0 ;shiftdir*sind(alpha),cosd(alpha), 0; 0,0,1];
+
+%rotate all coordinates by alpha, with A
+for i=1:length(chanlocs)
+   chan=A2*[chanlocs(i).X; chanlocs(i).Y;chanlocs(i).Z];
+   chanlocs(i).X=chan(1);
+   chanlocs(i).Y=chan(2);
+   chanlocs(i).Z=chan(3);
+end
+
+%% shift to the head model in the MNI plot
+% brain volume extrema of standard EEGLAB MNI head model around which to
+% fit current electrodes
+minVal = [-71, -108, -63];
+maxVal = [74, 77, 86];
+
+chanX=[chanlocs.X];
+chanY=[chanlocs.Y];
+chanZ=[chanlocs.Z];
+
+X = [min(chanX); max(chanX)];
+Y = [min(chanY); max(chanY)];
+Z = [min(chanZ); max(chanZ)];
+
+if (strcmp(atlas,'0to2')&&strcmp(layout,'2562'))||(strcmp(atlas,'4to8')&&strcmp(layout,'2562'))
+    % take second maximum instead of first, for datasets with wrong fiducials
+    X = [min(chanX(chanX>min(chanX))); max(chanX(chanX<max(chanX)))];
+    Y = [min(chanY(chanY>min(chanY))); max(chanY(chanY<max(chanY)))];
+    Z = [min(chanZ(chanZ>min(chanZ))); max(chanZ(chanZ<max(chanZ)))];
+end
+
+Xshift = (X(2)+X(1))/2+15;
+
+%shift between ears
+Yshift = (Y(2)+Y(1))/2;
+
+sf = (Y(2)-Yshift)/maxVal(2);       %scaling factor, obtained from Y
+
+Zshift = -((maxVal(3)*sf)-Z(2));
+
+for i=1:length(chanlocs)
+   chanlocs(i).X = chanlocs(i).X-Xshift; 
+   chanlocs(i).Y = chanlocs(i).Y-Yshift;
+   chanlocs(i).Z = chanlocs(i).Z-Zshift;
+end
+
+shift=[Xshift,Yshift,Zshift];
+
+%% completing chanlocs structure
+chans = convertlocs(chanlocs, 'cart2all');
+
+
+end
---
+++ leadfield/pha/lf_generate_frompha.m
@@ -0,0 +1,184 @@
+% lf = lf_generate_frompha(atlas, layout, varargin)
+%
+%       Generates a leadfield based on a Pediatric Head Atlas (v2),
+%       containing the electrodes indicated.
+%
+%       Note that the coordinates given by the original Pediatric Head 
+%       Atlases do not correspond to a standard coordinate space. This
+%       script calls fix_electrodes to largely correct for this by rotating
+%       and shifting both the electrode and the source coordinates to 
+%       approximately fit a standard MNI model. Since the Atlases are based
+%       on real recordings, the final given models may still not be 
+%       entirely symmetrical. You can use the plot_headmodel function to 
+%       verify this for yourself. It is particularly visible in the '2562' 
+%       layouts, where the electrodes cover the entire head shape. 
+%       
+%       Assumes you have the requested Pediatric Head Atlas directories in
+%       your MATLAB path. In particular, it needs the respective 
+%       LeadFieldMatrix_*, Sensors_*.txt, and Triples_Dipoles_* files. 
+%       As of 2018-02-05, the Pediatric Head Atlases are available upon 
+%       request at https://pedeheadmod.net
+%       
+%       Pediatric Head Atlas publication:
+%           Song, J., Morgan, K., Turovets, S., Li, K., Davey, C.,
+%           Govyadinov, P., Tucker, D. M. (2013). Anatomically accurate
+%           head models and their derivatives for dense array EEG source
+%           localization. Functional Neurology, Rehabilitation, and
+%           Ergonomics, 3(2-3), 275-293.
+%
+%       Parts of this script are based on the files provided in the
+%       Pediatric Head Atlas packages, authored by Pavel Govyadinov 
+%       (Electrical Geodesics Inc.) and David Hammond (NeuroInformatics
+%       Center, University of Oregon).
+%
+% In:
+%       atlas - string indicating the atlas to use: '0to2', '4to8', or
+%               '8to12'.
+%       layout - string indicating which layout (i.e. number of channels)
+%                to use: '128', '256', or '2562'. note: '256' is not 
+%                available for atlas '0to2'. 
+%
+% Optional inputs (key-value pairs):
+%       labels - cell of electrode labels to be used. default uses all
+%                available channels (including fiducials).
+%       montage - name of predefined channel montage. see utl_get_montage.
+%
+% Out:  
+%       lf - the leadfield containing the following fields
+%            .leadfield   - the leadfield, containing projections in three
+%                           directions (xyz) for each source, in a
+%                           nchannels x nsources x 3 matrix
+%            .orientation - a default orientation for each soure. for the
+%                           New York Head, this gives dipole orientations
+%                           perpendicular to the cortical surface
+%            .pos         - xyz MNI coordinates of each source
+%            .chanlocs    - channel information in EEGLAB format
+%
+% Usage example:
+%       >> lf = lf_generate_frompha('8to18', '2562')
+% 
+%                    Copyright 2018 Laurens R Krol
+%                    Team PhyPA, Biological Psychology and Neuroergonomics,
+%                    Berlin Institute of Technology
+
+% 2018-03-22 jpaw
+%   - Fixed electrode and source coordinates
+% 2018-02-05 First version
+
+% This file is part of Simulating Event-Related EEG Activity (SEREEGA).
+
+% SEREEGA is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+
+% SEREEGA is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+
+% You should have received a copy of the GNU General Public License
+% along with SEREEGA.  If not, see <http://www.gnu.org/licenses/>.
+
+function lf = lf_generate_frompha(varargin)
+
+% parsing input
+p = inputParser;
+
+addRequired(p, 'atlas', @ischar);
+addRequired(p, 'layout', @ischar);
+
+addParameter(p, 'labels', {}, @iscell);
+addParameter(p, 'montage', '', @ischar);
+
+parse(p, varargin{:})
+
+atlas = p.Results.atlas;
+layout = p.Results.layout;
+labels = p.Results.labels;
+montage = p.Results.montage;
+
+% setting file names
+if strcmp(layout, '2562')
+    % (file names are inconsistent for the 2562 layout)
+    leadfieldlayout = 'LargeSensorArray';
+    sensorlayout = 'generic';
+else
+    leadfieldlayout = layout;
+    sensorlayout = layout;
+end
+numchan = str2num(layout);
+
+sensorfile = sprintf('Sensors_%s_%s.txt', atlas, sensorlayout);
+dipolesfile = sprintf('Triples_Dipoles_%s', atlas);
+
+if strcmp(atlas, '4to8')
+    % (file names are inconsistent for the '4to8' atlas)
+    atlasf = '2to8';
+else
+    atlasf = atlas;
+end
+
+leadfieldfile = sprintf('LeadFieldMatrix_%s_%s', atlasf, leadfieldlayout);
+
+if any([exist(sensorfile), exist(dipolesfile), exist(leadfieldfile)] ~= 2)
+    error('SEREEGA:lf_generate_frompha:fileNotFound', 'Could not find the indicated Pediatric Head Atlas file(s) in the path.\nMake sure you have spelled the atlas and layout correctly, that you have obtained the files, and that MATLAB can find them.\nThey should be available at https://pedeheadmod.net')
+end
+
+% loading transformed electrode positions, removing fiducials and Cz
+[chanlocs, hm, A1, A2, shift] = fix_electrodes(atlas, layout);
+[~, chanidx] = ismember({'FidNz', 'FidT9', 'FidT10', 'Cz'}, {chanlocs.labels});
+chanidx = chanidx(~~chanidx);
+chanlocs(chanidx) = [];
+
+if ~isempty(montage)
+    % taking channel labels from indicated montage
+    labels = utl_get_montage(montage);
+elseif isempty(labels)
+    % taking all available EEG electrodes
+    labels = {chanlocs.labels};
+end
+
+% obtaining indices of indicated electrodes
+[~, chanidx] = ismember(labels, {chanlocs.labels});
+if any(~chanidx)
+    missingchans = find(~chanidx);
+    warning('\nElectrode %s not available', labels{missingchans});
+    chanidx(missingchans) = [];
+end
+chanlocs = chanlocs(chanidx);
+
+% getting lead field
+fid = fopen(leadfieldfile, 'r');
+leadfield = fread(fid, 'double', 'b');
+fclose(fid);
+leadfield = reshape(leadfield, 3, [], numchan);
+leadfield = permute(leadfield, [3, 2, 1]);
+leadfield = leadfield(chanidx,:,:);
+
+% getting dipoles
+fid = fopen(dipolesfile, 'r');
+dipoles = fread(fid, 3*size(leadfield,2)+1, 'int32', 0, 'l');
+dipoles = dipoles(2:end);
+fclose(fid);
+
+% dipole locations must be rotated and shifted the same way as the
+% electrodes. since readlocs reads the electrodes with X=Y and Y=-X, we
+% first read the dipoles in the same orientation.
+pos = [dipoles(2:3:end), -dipoles(1:3:end), dipoles(3:3:end)];
+for i=1:length(pos)
+    % rotating and shifting dipole to align with fixed electrodes;
+    pos(i,:) = (A2*A1*transpose(pos(i,:)-hm))-transpose(shift);
+    
+    % fixing the readlocs-interchange
+    pos(i,:) = [-pos(i,2), pos(i,1), pos(i,3)];
+end
+
+% preparing output
+lf = struct();
+lf.leadfield = leadfield;
+lf.orientation = zeros(size(leadfield, 2), 3);
+lf.pos = pos;
+lf.chanlocs = chanlocs;
+
+end
