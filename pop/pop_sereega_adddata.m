9aa420bf8b533a94c7cd52a6f886e0f6371a5a5a
Laurens R Krol <lrkrol+git@gmail.com>
2018-05-01 11:08:05 -0700

    Incremental GUI development

--- pop/pop_sereega_adddata.m
+++ pop/pop_sereega_adddata.m
@@ -1,6 +1,6 @@
-% EEG = pop_sereega_addnoise(EEG)
+% EEG = pop_sereega_adddata(EEG)
 %
-%       Pops up a dialog window that allows you to add a noise class to the
+%       Pops up a dialog window that allows you to add a data class to the
 %       simulation.
 %
 %       The pop_ functions serve only to provide a GUI for some of
@@ -10,7 +10,7 @@
 %       EEG - an EEGLAB dataset
 %
 % Out:  
-%       EEG - the EEGLAB dataset with a noise class added to 
+%       EEG - the EEGLAB dataset with a data class added to 
 %             EEG.etc.sereega.signals depending on the actions taken in the
 %             dialog window
 % 
@@ -18,7 +18,7 @@
 %                    Team PhyPA, Biological Psychology and Neuroergonomics,
 %                    Berlin Institute of Technology
 
-% 2018-04-30 First version
+% 2018-05-01 First version
 
 % This file is part of Simulating Event-Related EEG Activity (SEREEGA).
 
@@ -35,24 +35,30 @@
 % You should have received a copy of the GNU General Public License
 % along with SEREEGA.  If not, see <http://www.gnu.org/licenses/>.
 
-function EEG = pop_sereega_addnoise(EEG)
+function EEG = pop_sereega_adddata(EEG)
 
 if ~isfield(EEG.etc, 'sereega') || ~isfield(EEG.etc.sereega, 'signals')
     EEG.etc.sereega.signals = {};
 end
 
 [~, ~, ~, structout] = inputgui( ...
-        'geometry', { 1 1 [1 1 1 1] [1 1 1 1] [1 1 1 1] [1 1 1 1]}, ...
-        'geomvert', [1 1 1 1 1 1], ...
+        'geometry', { 1 1 [1 1 1 1] [1 1 2] [1 1 2] [1 1 2] 1 [1 1 1 1] [1 1 1 1]}, ...
+        'geomvert', [1 1 1 1 1 1 1 1 1], ...
         'uilist', { ...
-                { 'style', 'text', 'string', 'Add noise', 'fontweight', 'bold' }, ...
+                { 'style', 'text', 'string', 'Add data', 'fontweight', 'bold' }, ...
                 { }, ...
                 { 'style', 'text', 'string', 'Parameter', 'fontweight', 'bold' }, ...
                 { 'style', 'text', 'string', 'Base value *', 'fontweight', 'bold' }, ...
                 { 'style', 'text', 'string', 'Deviation', 'fontweight', 'bold' }, ...
                 { 'style', 'text', 'string', 'Slope', 'fontweight', 'bold' }, ...
-                { 'style', 'text', 'string', 'Color *' }, ...
-                { 'style', 'popupmenu', 'string', 'blue|blue-unif|brown|brown-unif|pink|pink-unif|purple|purple-unif|white|white-unif', 'tag', 'color' }, ...
+                { 'style', 'text', 'string', 'Data *' }, ...
+                { 'style', 'edit', 'string', 'varname', 'tag', 'data' }, ...
+                { }, ...
+                { 'style', 'text', 'string', 'Index *' }, ...
+                { 'style', 'edit', 'string', '{''e'', '':''}', 'tag', 'index' }, ...
+                { }, ...
+                { 'style', 'text', 'string', 'Amplitude type *' }, ...
+                { 'style', 'popupmenu', 'string', 'absolute|relative', 'tag', 'amplitudeType' }, ...
                 { }, ...
                 { }, ...
                 { 'style', 'text', 'string', 'Amplitude *' }, ...
@@ -62,32 +68,34 @@ end
                 { 'style', 'text', 'string', 'Probability' }, ...
                 { 'style', 'edit', 'string', '', 'tag', 'probability' }, ...
                 { }, ...
-                { 'style', 'edit', 'string', '', 'tag', 'probabilityslope' }, ...
-                
+                { 'style', 'edit', 'string', '', 'tag', 'probabilityslope' }, ...                
                 }, ... 
-        'helpcom', 'pophelp(''noise_check_class'');', ...
-        'title', 'Add noise');
+        'helpcom', 'pophelp(''data_check_data'');', ...
+        'title', 'Add data');
 
 if ~isempty(structout)
     % user pressed OK
-    noise = struct('type', 'noise');
+    data = struct('type', 'data');
     
-    colors = {'blue', 'blue-unif', 'brown', 'brown-unif', 'pink', 'pink-unif', 'purple', 'purple-unif', 'white', 'white-unif'};
-    if ~isempty(structout.color)
-        noise.color = colors{structout.color}; end
+    data.data = evalin('base', structout.data);
+    data.index = eval(structout.index);
+    
+    amplitudeType = {'absolute', 'relative'};
+    data.amplitudeType = amplitudeType{structout.amplitudeType};
+    
     if ~isempty(structout.amplitude)
-        noise.amplitude = str2num(structout.amplitude); end
+        data.amplitude = str2num(structout.amplitude); end
     if ~isempty(structout.amplitudedv)
-        noise.amplitudeDv = str2num(structout.amplitudedv); end
+        data.amplitudeDv = str2num(structout.amplitudedv); end
     if ~isempty(structout.amplitudeslope)
-        noise.amplitudeSlope = str2num(structout.amplitudeslope); end
+        data.amplitudeSlope = str2num(structout.amplitudeslope); end
     if ~isempty(structout.probability)
-        noise.probability = str2num(structout.probability); end
+        data.probability = str2num(structout.probability); end
     if ~isempty(structout.probabilityslope)
-        noise.probabilitySlope = str2num(structout.probabilityslope); end
+        data.probabilitySlope = str2num(structout.probabilityslope); end
     
-    noise = utl_check_class(noise);
-    EEG.etc.sereega.signals = [EEG.etc.sereega.signals, {noise}];
+    data = utl_check_class(data);
+    EEG.etc.sereega.signals = [EEG.etc.sereega.signals, {data}];
 end
 
 end
